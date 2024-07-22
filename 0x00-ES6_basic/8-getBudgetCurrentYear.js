490f1a667ddaa120d0c725c2e05236429f0f31e3
Dan <danlewismuriuki@gmail.com>
2024-07-22 22:31:57 +0300

    Implement generator function for iterating over all employees in report
    - Added `createIteratorObject` function to yield each employee from all departments.
    - Used ES6 generator function syntax to provide a custom iterator.
    - Ensured compliance with semi-standard style guide by fixing formatting issues.

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,20 +1,16 @@
-function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const budget = {
-  
-    
-    
-    [`income-${getCurrentYear()}`] : income,
-    [`gdp-${getCurrentYear()}`] : gdp,
-    [`capita-${getCurrentYear()}`] : capita,
+function getCurrentYear () {
+  const date = new Date();
+  return date.getFullYear();
+}
 
-    };
-  
+export default function getBudgetForCurrentYear (income, gdp, capita) {
+  const budget = {
 
-  
-    return budget;
-  }
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita
+
+  };
+
+  return budget;
+}
