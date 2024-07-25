48cf87ec38e427314ccb6b94fd61ebcff3baeea4
Dan <danlewismuriuki@gmail.com>
2024-07-25 14:40:44 +0300

    Implement Currency class with code, name, and displayFullCurrency method
    - Added a Currency class with private attributes _code and _name.
    - Implemented getters and setters for code and name with type validation.
    - Added displayFullCurrency method to return currency in the format 'name (code)'.
    - Created test script 3-main.js to verify the implementation.

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+export default class Currency {
+  constructor(code, name) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = value;
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('code must be a string');
+    } else {
+      this._name = value;
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
