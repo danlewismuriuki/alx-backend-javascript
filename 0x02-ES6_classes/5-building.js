332fa61576cbbdd94d4959fde26aec854a4a0583
Dan <danlewismuriuki@gmail.com>
2024-07-25 19:55:12 +0300

    feat: Implement abstract Building class with required method override checks
    - Define Building class with constructor to accept sqft attribute
    - Store sqft attribute as _sqft and implement a getter for it
    - Ensure Building class cannot be instantiated directly by throwing an error in the constructor
    - Require subclasses of Building to override the evacuationWarningMessage method, throwing an error if not overridden
    - Update 5-main.js to test proper instantiation and method override behavior

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+export default class Building{
+    constructor(sqft) {
+        if (new.target === Building){
+            throw new Error('Cannot instantiate Building directly');
+        }
+        this._sqft = sqft;
+    }
+    get sqft() {
+        return this._sqft;
+    }
+    
+    evacuationWarningMessage(){
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+}
