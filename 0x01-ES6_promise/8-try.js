852a875f15e8f1746d5cccc0140bd097faa19598
Dan <danlewismuriuki@gmail.com>
2024-07-26 18:45:38 +0300

    fix: handle division by zero in divideFunction
    - Throw an error with the message 'cannot divide by 0' if the denominator is zero.
    - Return the result of the division otherwise.

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
+
+// return the numerator divided by the denominator.
