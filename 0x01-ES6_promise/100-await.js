c5690d7abe63f9ddffdecf6e1234bd1d6170bf5a
Dan <danlewismuriuki@gmail.com>
2024-07-26 19:49:06 +0300

    Implement asyncUploadUser function to handle async operations
    - Imported `uploadPhoto` and `createUser` from `utils.js`.
    - Created `asyncUploadUser` function to call both functions asynchronously.
    - Returned an object containing the results of the two functions.
    - Added error handling to return an object with null values if any function fails.

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,20 @@
+import {uploadPhoto,createUser} from './utils';
+
+export default async function asyncUploadUser(){
+    
+    try{
+        const Photoresponse = await uploadPhoto()
+        const Userresponse = await createUser()
+
+        return {
+            photo: Photoresponse,
+            user: Userresponse,
+          }
+
+    } catch (error){
+        return {
+            photo: null,
+            user: null,
+        }
+    }
+}
