e6fc6244e3dced7940e3998549ed3fe7a669de5b
Dan <danlewismuriuki@gmail.com>
2024-07-26 13:26:48 +0300

    Implement handleProfileSignup function with corrected parameter name
    - Imported signUpUser and uploadPhoto functions.
    - Used Promise.allSettled to handle both promises and return results with status and values.
    - Fixed parameter name in uploadPhoto function call.

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName), // Corrected the parameter name here
-  ]).then((results) => results.map((result) => {
-    if (result.status === 'fulfilled') {
-      return {
-        status: result.status,
-        value: result.value,
-      };
-    }
-    return {
-      status: result.status,
-      value: result.reason,
-    };
-  }));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
