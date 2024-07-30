b79bd4dc90c60c0689de920f246edf09662f3322
Dan <danlewismuriuki@gmail.com>
2024-07-30 18:32:12 +0300

    Implement setFromArray function in 6-set.js
    - Added setFromArray function to convert an array to a Set, removing duplicate values
    - Verified functionality with example in 6-main.js
    - Output verified with correct Set format

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,11 +1,3 @@
-// export default function setFromArray (listArray) {
-//     const set = new Set(listArray);
-//     return set
-  
-// }
-
-// 6-set.js
 export default function setFromArray(array) {
     return new Set(array);
-  }
-  
+}
