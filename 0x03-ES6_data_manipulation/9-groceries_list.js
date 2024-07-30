effa894c8ed0622962c7e7c3e564814dfc1bc02c
Dan <danlewismuriuki@gmail.com>
2024-07-31 01:09:31 +0300

    feat(groceries): add function to create a grocery list map
    Added the `groceriesList` function which returns a map of grocery items and their quantities.
    This function helps in managing grocery items efficiently.
    
    Related issue: #42

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,16 @@
+// export default function groceriesList(){
+//     //that returns a map of groceries with the following items (name, quantity):
+
+// }
+
+// 9-groceries_list.js
+export default function groceriesList() {
+    return new Map([
+      ['Apples', 10],
+      ['Tomatoes', 10],
+      ['Pasta', 1],
+      ['Rice', 1],
+      ['Banana', 5]
+    ]);
+  }
+  
