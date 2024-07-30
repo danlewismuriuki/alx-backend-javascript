8c14b7b35f0431bbab0a1d79a4ed587b89f550ba
Dan <danlewismuriuki@gmail.com>
2024-07-31 01:12:37 +0300

    fix(slice): correct slice index for extracting substring
    Updated the slice method to ensure it correctly extracts the substring based on the start and end indices, including handling edge cases.
    
    Fixes: #18

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,16 +1,9 @@
-// export default function groceriesList(){
-//     //that returns a map of groceries with the following items (name, quantity):
-
-// }
-
-// 9-groceries_list.js
 export default function groceriesList() {
-    return new Map([
-      ['Apples', 10],
-      ['Tomatoes', 10],
-      ['Pasta', 1],
-      ['Rice', 1],
-      ['Banana', 5]
-    ]);
-  }
-  
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
