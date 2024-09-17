16b5667df914b2ad6f6b39535f53e6f05ea75036
Dan <danlewismuriuki@gmail.com>
2024-09-17 13:38:11 +0300

    Fix typo in HTTP server code: change res.WriteHead to res.writeHead
    Corrected the method name from `res.WriteHead` to `res.writeHead` to properly set response headers. This resolves the TypeError encountered during server startup.

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+const http = require("http")
+
+const host = 'localhost';
+const port = 1245;
+
+
+const app = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!')
+});
+
+app.listen(port, () => {
+    // console.log(`server is running on port ${port}`);
+});
+
+module.exports = app;
