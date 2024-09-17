87f1e77eadb63747e45d9dd1af5de17d637b1208
Dan <danlewismuriuki@gmail.com>
2024-09-17 13:43:18 +0300

    Update HTTP server to use 127.0.0.1 as host
    Changed the server's host from 'localhost' to '127.0.0.1' in `4-http.js` to specify the loopback IP address. The server continues to respond with "Hello Holberton School!" on port 1245. Removed console logging for a cleaner output.

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,15 @@
-const http = require("http")
+const http = require('http');
 
-const host = 'localhost';
+const host = '127.0.0.1';
 const port = 1245;
 
-
 const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!')
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
 });
 
-app.listen(port, () => {
-    // console.log(`server is running on port ${port}`);
+app.listen(port, host, () => {
+  // console.log(`server is running on port ${port}`);
 });
 
 module.exports = app;
