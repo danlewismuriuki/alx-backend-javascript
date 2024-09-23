d69c44dffa29222ddd98087def4a59abfef493c3
lsd <danlewismuriuki2@gmail.com>
2024-09-24 01:14:54 +0300

    Add tests for sendPaymentRequestToApi
    - Implement unit tests for sendPaymentRequestToApi using Sinon to stub Utils.calculateNumber.
    - Verify that the correct total is logged for different input values (100, 20) and (10, 10).
    - Ensure the tests check that the stubbed function is called with the expected arguments.

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -13,9 +13,11 @@ describe('sendPaymentRequestToApi', function () {
 
     afterEach(function () {
         consoleLogSpy.restore();
+        sinon.restore();
     });
 
     it('should log the correct total for inputs 100 and 20', function () {
+        sinon.stub(Utils, 'calculateNumber').returns(120);
         sendPaymentRequestToApi(100, 20);
         expect(consoleLogSpy.calledOnce).to.be.true;
         expect(consoleLogSpy.calledWith('The total is: 120')).to.be.true;
@@ -23,6 +25,7 @@ describe('sendPaymentRequestToApi', function () {
 
 
     it('should log the correct total for inputs 10 and 10', function () {
+        sinon.stub(Utils, 'calculateNumber').returns(20);
         sendPaymentRequestToApi(10, 10);
         expect(consoleLogSpy.calledOnce).to.be.true;
         expect(consoleLogSpy.calledWith('The total is: 20')).to.be.true;
